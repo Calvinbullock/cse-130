b9b5788d4f712f5e5675ffbcc484ddcf1572b151
Calvinbullock <calvinnbullock@gamil.com>
2023-04-27 10:42:49 -0600

    started lab 02

--- lab_02.py
+++ lab_02.py
@@ -7,7 +7,7 @@
 # 3. Assignment Description:
 #    This program will take input for a password and user name and compare it
 #    the usernames and passwords stored in a json. If the password matches the 
-#    password stored for that user name it will allow accses.
+#    password stored for that user name it will allow acsses.
 # 
 # 4. What was the hardest part? Be as specific as possible.
 #    Working out how to interact with the json file. Both getting the file to 
@@ -24,6 +24,12 @@ def Authentication(user_name_in, password_in):
     """
     This functions check if the password matches the user.
     It takes two strings and returns a bool.
+
+    user_name_in (string)
+    password_in  (String)
+
+    returns:
+    A bool
     """
 
     # Double loop from:
@@ -46,29 +52,36 @@ def Login_Log(authorized):
     else:
         print("You are not authorized to use the system.")
 
-# Opens the file Lab02,json and converts it to python dictinary.
-file = open("/home/calvin/Documents/MainCode/cse-130/Lab02.json")
-data = file.read()
-dict = json.loads(data)
+# checks if the the file opens and if the dictinary can be used.
+try:
+    # Opens the file Lab02,json and converts it to python dictinary.
+    file = open("Lab02.json")
+    data = file.read()
+    dict = json.loads(data)
 
-#Normal input code
-""" 
-# Takes input from the user for password and username.
-user_name_in = input("Username: ")
-password_in = input("Password: ")
+    #Normal input code
+    """ 
+    # Takes input from the user for password and username.
+    user_name_in = input("Username: ")
+    password_in = input("Password: ")
 
-# Passes the username and password input by the user and checks them. 
-Login_Log(Authentication(user_name_in, password_in))
-"""
+    # Passes the username and password input by the user and checks them. 
+    Login_Log(Authentication(user_name_in, password_in))
+    """
 
-# Auto test cases
-print("Username: John Cheese, Password: None shall pass")
-Login_Log(Authentication("John Cheese", "None shall pass"))
-print()
+    # Auto test cases
+    print("Username: John Cheese, Password: None shall pass")
+    Login_Log(Authentication("John Cheese", "None shall pass"))
+    print()
 
-print("Username: Black Knight, Password: Tis but a scratch.")
-Login_Log(Authentication("Black Knight", "Tis but a scratch"))
-print()
+    print("Username: Black Knight, Password: Tis but a scratch.")
+    Login_Log(Authentication("Black Knight", "Tis but a scratch"))
+    print()
 
-print("Username: Black Knight, Password: None shall pass.")
-Login_Log(Authentication("Black Knight", "None shall pass"))
+    print("Username: Black Knight, Password: None shall pass.")
+    Login_Log(Authentication("Black Knight", "None shall pass"))
+
+    file.close()
+
+except:
+    print("Data unreachable")
