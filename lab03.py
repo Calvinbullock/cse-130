d611c5a50b016db2a2a8bed715c0c92df2ca9a47
Calvinbullock <calvinnbullock@gamil.com>
2023-05-10 15:34:34 -0600

    fixed a typo in the prep_phrase return

---
+++ lab03.py
@@ -0,0 +1,129 @@
+# 1. Name:
+#      Calvin Bullock
+# 2. Assignment Name:
+#      Lab 04: Monopoly
+# 3. Assignment Description:
+#      -describe what this program is meant to do-
+# 4. What was the hardest part? Be as specific as possible.
+#      Was it the syntax of Python?
+#      Was it an aspect of the problem you are to solve?
+#      Was it the instructions or any part of the problem definition?
+#      Was it the submission process?
+# 5. How long did it take for you to complete the assignment?
+#      -total time in hours including reading the assignment and
+#       submitting the program-
+
+
+colour_prompt = input("Do you own all the green properties? (y/n): ")
+
+if colour_prompt.lower() == "y":
+    prompt_p_a = input(
+        "What is on Pennsylvania Avenue? (0:nothing, 1:one house, ... 5:a hotel) "
+    )
+    prompt_p_a = int(prompt_p_a)
+
+    print("line 23")
+    if prompt_p_a != 5:
+        prompt_n_c = input(
+            "What is on North Carolina Avenue? (0:nothing, 1:one house, ... 5:a hotel) "
+        )
+        prompt_n_c = int(prompt_n_c)
+        print("line 27")
+
+        if prompt_n_c != 5:
+            prompt_pc = input(
+                "What is on Pacific Avenue? (0:nothing, 1:one house, ... 5:a hotel) "
+            )
+            prompt_pc = int(prompt_pc)
+
+            if prompt_pc != 5:
+                hotels = input("How many hotels are there to purchase? ")
+                hotels = int(hotels)
+
+                if hotels > 0:
+                    num_house_PA_need = 0
+                    num_house_NC_need = 0
+                    num_house_PC_need = 0
+                    num_house_total_need = 0
+                    total_money_need = 0
+
+                    wallet_size = input("How much cash do you have to spend? ")
+
+                    if total_money_need > wallet_size:
+                        houses = input("How many houses are there to purchase?")
+
+                        if houses < num_house_total_need:
+                            if num_house_NC_need > 0:
+                                print(
+                                    "This will cost $[price].\
+                                        Purchase 1 hotel and [number of houses] house(s).\
+                                        Put 1 hotel on Pennsylvania and return any houses to the bank.\
+                                        Put [number of houses] house(s) on North Carolina.\
+                                        Put [number of houses] house(s) on Pacific."
+                                )
+                                print(
+                                    "This will cost $[price].\
+                                        Purchase 1 hotel and [number of houses] house(s).\
+                                        Put 1 hotel on Pennsylvania and return any houses to the bank.\
+                                        Put [number of houses] house(s) on North Carolina."
+                                )
+
+                            elif num_house_PC_need > 0:
+                                print(
+                                    "This will cost $[price].\
+                                        Purchase 1 hotel and [number of houses] house(s).\
+                                        Put 1 hotel on Pennsylvania and return any houses to the bank.\
+                                        Put [number of houses] house(s) on North Carolina.\
+                                        Put [number of houses] house(s) on Pacific."
+                                )
+                                print(
+                                    "This will cost $[price].\
+                                         Purchase 1 hotel and [number of houses] house(s).\
+                                         Put 1 hotel on Pennsylvania and return any houses to the bank.\
+                                         Put [number of houses] house(s) on Pacific."
+                                )
+
+
+# print("You do not have sufficient funds to purchase a hotel at this time.")
+
+# print("There are not enough houses available for purchase at this time.")
+# print("There are not enough hotels available for purchase at this time.")
+# print(
+#     "You cannot purchase a hotel until you own all the properties of a given color group."
+# )
+
+# print("You cannot purchase a hotel if the property already has one.")
+# print("Swap North Carolina's hotel with Pennsylvania's 4 houses.")
+# print("Swap Pacific's hotel with Pennsylvania's 4 houses.")
+
+# A
+# print(
+#     "This will cost $[price].\
+#          Purchase 1 hotel and [number of houses] house(s).\
+#          Put 1 hotel on Pennsylvania and return any houses to the bank.\
+#          Put [number of houses] house(s) on North Carolina.\
+#          Put [number of houses] house(s) on Pacific."
+# )
+
+# B
+# print(
+#     "This will cost $[price].\
+#          Purchase 1 hotel and [number of houses] house(s).\
+#          Put 1 hotel on Pennsylvania and return any houses to the bank.\
+#          Put [number of houses] house(s) on North Carolina."
+# )
+
+# C
+# print(
+#     "This will cost $[price].\
+#          Purchase 1 hotel and [number of houses] house(s).\
+#          Put 1 hotel on Pennsylvania and return any houses to the bank.\
+#          Put [number of houses] house(s) on Pacific."
+# )
+
+# D
+# print(
+#     "This will cost $[price].\
+#          Purchase 1 hotel and [number of houses] house(s).\
+#          Put 1 hotel on Pennsylvania and return any houses to the bank."
+# )
