9a549a0a1d09af03b8dfafc78c8602c94b7eff31
Calvinbullock <calvinnbullock@gamil.com>
2023-06-19 11:24:39 -0600

    week 10 comments!

--- week10/lab10.py
+++ week10/lab10.py
@@ -1,20 +1,23 @@
 # 1. Name:
-#      -your name-
+#      Calvin Bullock
 # 2. Assignment Name:
 #      Lab 10: Numeric Sequence
 # 3. Assignment Description:
-#      -describe what this program is meant to do-
+#      This program computes the nth François number.
 # 4. What was the hardest part? Be as specific as possible.
-#      -a paragraph or two about how the assignment went for you-
+#      -TODO a paragraph or two about how the assignment went for you-
 # 5. How long did it take for you to complete the assignment?
-#      -total time in hours including reading the assignment and submitting the program-
+#      -TODO total time in hours including reading the assignment and submitting the program-
+
 
 def main():
+    # TRUE RUN
     if __debug__ == False:
         target_i = input("for target index: ")
         target_i = int(target_i)
         francois_number(target_i)
 
+    # DEBUG TEST cases
     if __debug__:
         francois_number(-1)
         francois_number(0)
@@ -31,13 +34,17 @@ def francois_number(target_i):
     count = 1
     francois_array = [2, 1]
 
+    # if the target is less then 2 term.
     if target_i < 2:
         print(francois_array[target_i])
         print()
 
+    # if the target is further then the 3rd term.
     else:
         while count < target_i:
+            # Take the two values stored in teh array and add them together
             temp_value = francois_array[0] + francois_array[1]
+            # Move the array values back with the temp becomeing the 2nd value
             francois_array[0] = francois_array[1]
             francois_array[1] = temp_value
 
@@ -45,6 +52,5 @@ def francois_number(target_i):
 
         print(francois_array[1])
         print()
-
 
 main()
