c5acf603be5581b1eb94617a9a93daa519222c34
Calvinbullock <calvinnbullock@gamil.com>
2023-07-05 16:30:05 -0600

    commenting clean up

--- week12/prime-psodo.py
+++ week12/prime-psodo.py
@@ -1,19 +1,21 @@
 # Calvin Bullock
 
+# -- Promgram --
+# Compute prime numbs to the nth term with out modulo
+
 import math
 
 # Quick visuwal test
 # n = 20 { 2, 3, 5, 7, 11, 13, 17, 19 }
 # n = 100 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.
 
-# # Quick Testing
+# # TODO Quick Testing
 # target_value = 25
-target_value = 100
+# target_value = 100
 
-# BUG remove and reinstate input
 # Take user input
-# target_value = input("Enter your nth number you would like to compute until: ")
-# target_value = int(target_value)
+target_value = input("Enter your nth number you would like to compute until: ")
+target_value = int(target_value)
 
 count_list = []
 
