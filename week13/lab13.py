d8ae06b49922242aaaefa63eac3bf816efc37abb
Calvinbullock <calvinnbullock@gamil.com>
2023-07-12 15:35:18 -0600

    week 13 working

--- week13/lab13.py
+++ week13/lab13.py
@@ -7,16 +7,19 @@ import math
 #     if numbers[factor]:
 #         for multiple = factor * 2 ... number + 1 by factor
 #             numbers[multiple] = False
-        
+
 # for index = 2 ... number
 #     if numbers[index]
 #         primes.push_back(index)
 
 numbers = []
 
-target_num = input("Enter nth term to check")
-target_num += int(target_num)
+# target_num = input("Enter nth term to check")
+# target_num = int(target_num)
 
+target_num = 20
+
+# Build list
 for num in range(0, target_num):
     numbers.append(True)
 
@@ -24,12 +27,14 @@ for num in range(0, target_num):
 numbers[0] = False
 numbers[1] = False
 
-
-for factor in range (2, math.sqrt(target_num + 1)):
+# First loop check every value
+for factor in range(2, int(math.sqrt(target_num) + 1)):
+    # If value is true, check it
     if numbers[factor]:
-        for multiple in range(factor * 2, target_num + 1 by factor):
-            numbers[multiple] = False
-        
+        #
+        for multiple in range(factor * 2, target_num, factor):  # by factor
+            numbers[multiple] = False  # falls out of range...
+
 for index in range(2, target_num):
     if numbers[index]:
-        primes.push_back(index)
+        print(index)
