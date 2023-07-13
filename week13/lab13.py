9acf7e38c62cc19852341da8bac5ee3814147814
Calvinbullock <calvinnbullock@gamil.com>
2023-07-12 18:53:03 -0600

    commenting and clean up

--- week13/lab13.py
+++ week13/lab13.py
@@ -1,40 +1,92 @@
+# 1. Name:
+#      Calvin BUllock-
+# 2. Assignment Name:
+#      Lab 13: Prime Numbers
+# 3. Assignment Description:
+#      This Program will calculate prime to the nth number.
+# 4. What was the hardest part? Be as specific as possible.
+#      -a paragraph or two about how the assignment went for you-
+# 5. How long did it take for you to complete the assignment?
+#      2 Hours
+
 import math
 
-# numbers = [True, True]
-# numbers[0] = numbers[1] = False
 
-# for factor = 2 .... math.sqrt(number+1):
-#     if numbers[factor]:
-#         for multiple = factor * 2 ... number + 1 by factor
-#             numbers[multiple] = False
+def main():
+    # Testing
+    # prime_iterater(-1)
+    # print()
+    # prime_iterater(0)
+    print()
+    prime_iterater(1)
+    print()
+    prime_iterater(2)
+    print()
+    prime_iterater(10)
+    print()
+    prime_iterater(53)
+    print()
+    prime_iterater(100)
+    print()
+    prime_iterater(200)
+    print()
 
-# for index = 2 ... number
-#     if numbers[index]
-#         primes.push_back(index)
+    # User Run
+    exit = True
+    target_num = 0
 
-numbers = []
+    # Run until valid target entered
+    while exit:
+        target_num = input("Enter nth term to check: ")
+        target_num = int(target_num)
 
-# target_num = input("Enter nth term to check")
-# target_num = int(target_num)
+        if target_num > 0:
+            exit = False
 
-target_num = 20
+        else:
+            print("Please enter number greater then zero")
 
-# Build list
-for num in range(0, target_num):
-    numbers.append(True)
+    prime_iterater(target_num)
 
-# Initially false
-numbers[0] = False
-numbers[1] = False
 
-# First loop check every value
-for factor in range(2, int(math.sqrt(target_num) + 1)):
-    # If value is true, check it
-    if numbers[factor]:
-        #
-        for multiple in range(factor * 2, target_num, factor):  # by factor
-            numbers[multiple] = False  # falls out of range...
+def prime_iterater(target_num):
+    # This function computes prime numbers up to the target_num.
+    #
+    # Paramiters:
+    #    [Target_num: int] - the nth term to cumpute prime up to
+    #
+    numbers = []
 
-for index in range(2, target_num):
-    if numbers[index]:
-        print(index)
+    # Build list
+    for num in range(0, target_num + 1):
+        numbers.append(True)
+
+    # Initially false
+    numbers[0] = False
+    numbers[1] = False
+
+    # First loop check every value
+    for factor in range(2, int(math.sqrt(target_num) + 1)):
+        # If value is true, check it
+        if numbers[factor]:
+            # Iterate along factore
+            for multiple in range(factor * 2, target_num, factor):  # by factor
+                numbers[multiple] = False  # falls out of range...
+
+    # Print out primes in list
+    for index in range(2, target_num):
+        if numbers[index]:
+            print(f"{index}, ", end="")
+
+    print()
+
+
+main()
+
+
+# Visuwal checking
+# 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+# 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
+# 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
+# 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
+# 263, 269, 271, 277, 281, 283, 293.
